6d7fe9fe2773f562d6b386464801dbefc27a3f95
Pawan Singh <pmarsingh@gmail.com>
2021-07-23 10:31:28 +0530

    First version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const fs = require("fs");
 const data = require("./assets/csc.json");
 /* const data = require("./assets/data");
- */ const port = 3000;
+ */ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
